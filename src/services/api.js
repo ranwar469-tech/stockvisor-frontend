968b3b76965b338e07f50dfa15dfed4248bef8c3
ranwar469-tech <261976913+ranwar469-tech@users.noreply.github.com>
2026-04-18 16:31:08 +0200

    new api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000'  /*'https://stockvisor-backend.onrender.com'*/ ,
+  baseURL: import.meta.env.VITE_API_URL || 'https://frigidity-bunkbed-stalemate.ngrok-free.dev/' /*'http://localhost:8000'*/  /*'https://stockvisor-backend.onrender.com'*/ ,
   headers: { 'Content-Type': 'application/json' },
 });
 
